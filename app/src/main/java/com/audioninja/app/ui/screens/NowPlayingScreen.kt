284bda8fdc03186c7ad6d89d46c3e306189c47a4
prowiderislam-gif <prowiderislam@gmail.com>
2026-08-09 22:37:46 +0530

    Update NowPlayingScreen.kt

--- app/src/main/java/com/audioninja/app/ui/screens/NowPlayingScreen.kt
+++ app/src/main/java/com/audioninja/app/ui/screens/NowPlayingScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
